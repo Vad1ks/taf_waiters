8f516e2ae3a01d95415f1db0927bd0299975a330
vadym sosnovenko <vadiksosnovenko@gmail.com>
2023-02-03 15:34:40 +0200

    deleted comments

--- tests/fluent_wait_test.py
+++ tests/fluent_wait_test.py
@@ -6,10 +6,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 def test_wait(driver):
     driver.get("https://gokcecapital.com/free-land")
-    # Fluent wait: will wait for a max of n sec until a specific element is located (type of explicit wait)
-    # Intelligent wait used with expected_conditions confined to one web element
-    # Can specify poll frequency and list of ignored exceptions
-    # Don't mix implicit & explicit/fluent waits, it can cause unpredictable wait times
+
     WebDriverWait(driver, timeout=15, poll_frequency=2,
                   ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException]) \
         .until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[@id="bottom-box-text-p"]')))
